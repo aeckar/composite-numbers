6e6597e60420b8ce013d26f78dcaf0d11e4737c1
ladle <eckardt.angel7@outlook.com>
2024-06-13 02:30:14 -0400

    Revised Cumulative documentation

--- src/main/kotlin/io/github/aeckar/composite/Cumulative.kt
+++ src/main/kotlin/io/github/aeckar/composite/Cumulative.kt
@@ -5,27 +5,19 @@ package io.github.aeckar.composite
  *
  * Applies to operations that return only their caller or a value returned
  * by [valueOf][CompositeNumber.valueOf], or some variant of it.
- * Any operation that breaks this contract is not considered cumulative.
- * Furthermore, overrides of functions with this annotation must also be cumulative.
+ * Any operation that breaks this contract cannot be considered cumulative.
+ * Overrides of cumulative functions must abide by the same contract.
  *
  * Mutability of composite numbers is necessary to keep allocations
  * to a minimum when performing intermediate operations.
  *
- * Instead of supplying separate functions for both, the business logic of operations
- * returning a unique value and those performed in-place are shared within the same function.
- * To determine what kind of result should be returned, the returned instance is determined by
- * `valueOf`, which:
- * - For immutable instances, returns a new immutable instance with the desired state
- * - For mutable instances, the same instance with a modified state
+ * Instead of supplying separate functions for returning a unique value and modifying an existing value,
+ * the business logic for both is shared within the same function.
+ * To determine which will be returned, this responsibility is delegated to `valueOf`.
  *
  * There is no guarantee that the integrity of the state of a cumulative integer
  * will be maintained if it is passed as an argument to an operation.
- *
- * The following always hold true:
- * - If the caller is `immutable`, the result is immutable
- * - If the caller is `mutable`, the result may be immutable or refer to the same instance
- * - If a mutable caller invokes a [cumulative][Cumulative] operation, the result is the same instance
- * - Mutable integers must never be passed as more than one parameter to an operation
+ * It is for this reason that users are not allowed to interact with mutable composite numbers at all.
  * @see MutableInt128
  * @see MutableRational
  */
